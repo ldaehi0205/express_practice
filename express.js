1d43cdc421bb74053a8dfa0f5f18acae3b6e1c2d
이대희 <ldaehi0205@gmail.com>
2021-12-07 23:39:13 +0900

    2nd Commit

--- express.js
+++ express.js
@@ -1,15 +1,21 @@
 const http = require("http");
 const express = require("express");
 const { sendPosts, endPointMessage } = require("./functions");
-
+const cors = require("cors");
 const app = express();
 
+app.use(express.json());
+
+app.use(cors());
 app.get("/", endPointMessage);
 
 app.get("/product", sendPosts);
 
-const server = http.createServer(app);
-const PORT = 8000;
-server.listen(PORT, () => {
-  console.log(`${PORT} on!`);
+app.post("/add", function (req, res) {
+  console.log(req.body);
+  res.send("완료");
+});
+
+app.listen(8000, function () {
+  console.log("listening on 8080");
 });
