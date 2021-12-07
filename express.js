62f428c61c2e9fccc6746b8231e91c76ae0ad8ad
이대희 <ldaehi0205@gmail.com>
2021-12-08 01:18:54 +0900

    .

--- express.js
+++ express.js
@@ -3,19 +3,45 @@ const express = require("express");
 const { sendPosts, endPointMessage } = require("./functions");
 const cors = require("cors");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+
+let db;
+MongoClient.connect(
+  "mongodb+srv://daehee:ldh642@cluster0.gvc4d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  function (error, client) {
+    if (error) return console.log(error);
+
+    db = client.db("myFirstDatabase");
+
+    // db.collection("users").insertOne(
+    //   { 이름: "John", _id: 100 },
+    //   function (에러, 결과) {
+    //     console.log("저장완료");
+    //   }
+    // );
+
+    app.listen(8000, function () {
+      console.log("listening on 8000");
+    });
+  }
+);
 
 app.use(express.json());
 
 app.use(cors());
-app.get("/", endPointMessage);
+// app.get("/", endPointMessage);
+app.get("/", function (req, res) {
+  db.collection("users")
+    .find()
+    .toArray(function (error, result) {
+      console.log(result);
+      res.status(200).json(result);
+    });
+});
 
 app.get("/product", sendPosts);
 
 app.post("/add", function (req, res) {
   console.log(req.body);
   res.send("완료");
-});
-
-app.listen(8000, function () {
-  console.log("listening on 8080");
 });
