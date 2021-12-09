6c04c815bc268fb6e204cba92d55b7b3c6bde8bf
이대희 <ldaehi0205@gmail.com>
2021-12-09 23:34:58 +0900

    .

--- express.js
+++ express.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 
+// app.get("/", endPointMessage);
 let db;
 MongoClient.connect(
   "mongodb+srv://daehee:ldh642@cluster0.gvc4d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -13,13 +14,6 @@ MongoClient.connect(
 
     db = client.db("myFirstDatabase");
 
-    // db.collection("users").insertOne(
-    //   { 이름: "John", _id: 100 },
-    //   function (에러, 결과) {
-    //     console.log("저장완료");
-    //   }
-    // );
-
     app.listen(8000, function () {
       console.log("listening on 8000");
     });
@@ -27,9 +21,8 @@ MongoClient.connect(
 );
 
 app.use(express.json());
-
 app.use(cors());
-// app.get("/", endPointMessage);
+
 app.get("/", function (req, res) {
   db.collection("users")
     .find()
@@ -39,9 +32,23 @@ app.get("/", function (req, res) {
     });
 });
 
+app.post("/add", function (req, res) {
+  let number = 0;
+  // console.log(req.body, "[[[[[");
+  db.collection("userCount").findOne({ name: "postNumber" }, (err, res) => {
+    number = res;
+  });
+  db.collection("post").insertOne(
+    { _id: number.total + 9, name: req.body.name },
+    () => {
+      console.log("저장완료");
+    }
+  );
+});
+
 app.get("/product", sendPosts);
 
-app.post("/add", function (req, res) {
-  console.log(req.body);
-  res.send("완료");
-});
+// app.post("/add", function (req, res) {
+//   console.log(req.body);
+//   res.send("완료");
+// });
